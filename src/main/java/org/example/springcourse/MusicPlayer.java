3d4add5d01fbdf5e026e469566be54f9a0dfb619
Ihor <ihor.cherednychenko@nure.ua>
2023-10-30 15:08:24 +0200

    Dependency Injection II. Homework

--- src/main/java/org/example/springcourse/MusicPlayer.java
+++ src/main/java/org/example/springcourse/MusicPlayer.java
@@ -1,12 +1,46 @@
 package org.example.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+
+    private String name;
+    private int volume;
 
     // IoC
-    public MusicPlayer(Music music) {this.music = music;}
+//    public MusicPlayer(Music music) {this.music = music;}
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public MusicPlayer() {}
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
